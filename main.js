527ea28d7ace81803a988daf4bf6665c56847a1e
Megha <75901998+017megha@users.noreply.github.com>
2021-05-12 17:32:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX =0;
+function setup()
+{
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log('posenet is initialized');
+}
+function gotPoses(results)
+{
+    if(results. length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        righttWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX) ;
+
+        console.log("leftWristX = "+ leftWristX+ "rightWristX = " + rightWristX + "difference = "+ difference);
+    }
+}
+function draw()
+{
+    background('#969A97');
+
+    document.getElementById("peter_side").innerHTML = "width and height of peter will be =" + difference  + "px";
+    fill('#FF0000');
+    stroke('#F90093');
+    text( "peter", noseX, noseY);
+}
